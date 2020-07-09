eb987e7a74be51fa5ed17b7362b89c79809724e1
kongseokhwan@gmail.com <kongseokhwan@gmail.com>
2020-07-09 21:14:38 +0900

    commit

--- pkg/client/ovs_prom_client.go
+++ pkg/client/ovs_prom_client.go
@@ -290,9 +290,6 @@ func (c *OVSClient) NtopQueryWithRate(rankSize int, metric string, duration stri
 	// Make Query String
 	query := fmt.Sprintf(ntopQueryWithRate, rankSize, metric, duration)
 
-	fmt.Printf("Debug nTop : %s", query)
-	fmt.Printf("Debug nTop : %s", query)
-
 	// Call ovsAPIQueryRange() & return result
 	return topkAPIQuery(c.Host, c.Port, query)
 }
